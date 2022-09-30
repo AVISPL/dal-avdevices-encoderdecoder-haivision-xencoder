f8c16ddbed2683514dc7f0388cd2a9265eb57467
Maksym.Rossiytsev <maksym.rossiitsev@avispl.com>
2022-09-30 18:42:22 +0300

    SYAL-1075 Makito X Encoder historical properties

---
+++ src/main/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/xencoder/statistics/DynamicStatisticsDefinitions.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 AVI-SPL Inc. All Rights Reserved.
+ */
+package com.avispl.symphony.dal.avdevices.encoderdecoder.haivision.xencoder.statistics;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * DynamicStatisticsDefinitions contains definitions for Dynamic property candidates.
+ *
+ * @author Maksym.Rossiitsev / Symphony Dev Team<br>
+ * @since 1.1.0
+ * */
+public enum DynamicStatisticsDefinitions {
+    Temperature("Temperature"),
+    EncodedBitrate("EncodedBitrate (kbps)"),
+    EncoderLoad("EncoderLoad (%)"),
+    Bitrate("Bitrate (kbps)"),
+    Buffer("Buffer (ms)"),
+    Latency("Latency (ms)"),
+    MaxBandwidth("MaxBandwidth (kbps)"),
+    RTT("RTT (ms)");
+
+    private final String name;
+    DynamicStatisticsDefinitions(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves {@link #name}
+     *
+     * @return value of {@link #name}
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Check if dynamic property definition exists, by name.
+     *
+     * @param name of the property to check
+     * @return true if definition exists, false otherwise
+     * */
+    public static boolean checkIfExists(String name) {
+        Optional<DynamicStatisticsDefinitions> dynamicStatisticsProperty = Arrays.stream(DynamicStatisticsDefinitions.values()).filter(c -> name.endsWith(c.getName())).findFirst();
+        if (dynamicStatisticsProperty.isPresent()) {
+            return true;
+        }
+        return false;
+    }
+}
